9f4a0de833e8a0b4b7c696c70dc179fcc71055da
MemduhAlpsoy <240101009@ogrenci.yalova.edu.tr>
2025-05-01 17:31:27 +0300

    Update config.php

--- YENİ KOD/inc/config.php
+++ YENİ KOD/inc/config.php
@@ -2,7 +2,7 @@
 <?php
 // Veritabanı ayarları
 define('DB_HOST', '127.0.0.1');
-define('DB_NAME', 'mahallem');
+define('DB_NAME', 'test');
 define('DB_USER', 'root');
 define('DB_PASS', '');
 define('DB_CHAR', 'utf8mb4');
